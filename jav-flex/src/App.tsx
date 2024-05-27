d53e825f8ac03e1833456024448f9fc60d9b0971
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2024-05-27 13:58:24 +0000

    agregando codigo

--- jav-flex/src/App.tsx
+++ jav-flex/src/App.tsx
@@ -3,10 +3,10 @@ import { useState } from "react"
 
 function App() {
   const [flex,setFlex]=useState(false)
-  const [gap,setGap]=useState('0')
-  const [direction,setDirection]=useState('flex-row')
-  const [justify,setJustify]=useState('justify-start')
-  const [items,setItems]=useState('items-start')
+  const [gap,setGap]=useState('')
+  const [direction,setDirection]=useState('')
+  const [justify,setJustify]=useState('')
+  const [items,setItems]=useState('')
   return (
     <div className="bg-gray-800 text-gray-200 px-4 py-4 h-screen flex flex-col gap-8">
       <nav>
@@ -23,17 +23,17 @@ function App() {
           </fieldset>
           <fieldset className="flex gap-2">
             <select onChange={(event)=>setGap(event.target.value)} name="" id="" className="px-0.5 py-1 w-32 bg-gray-600 shadow-lg">
-              <option value="0">gap-0</option>
-              <option value="1">gap-1</option>
-              <option value="2">gap-2</option>
-              <option value="3">gap-3</option>
-              <option value="4">gap-4</option>
-              <option value="5">gap-5</option>
-              <option value="6">gap-6</option>
-              <option value="7">gap-7</option>
-              <option value="8">gap-8</option>
-              <option value="9">gap-9</option>
-              <option value="10">gap-10</option>
+              <option value="gap-0">gap-0</option>
+              <option value="gap-1">gap-1</option>
+              <option value="gap-2">gap-2</option>
+              <option value="gap-3">gap-3</option>
+              <option value="gap-4">gap-4</option>
+              <option value="gap-5">gap-5</option>
+              <option value="gap-6">gap-6</option>
+              <option value="gap-7">gap-7</option>
+              <option value="gap-8">gap-8</option>
+              <option value="gap-9">gap-9</option>
+              <option value="gap-10">gap-10</option>
             </select>
           </fieldset>
           <fieldset className="flex gap-2">
@@ -63,12 +63,29 @@ function App() {
             </select>
           </fieldset>
         </form>     
-        <section className={`bg-gray-600 font-bold text-xl h-[400px] w-[800px] border-8 ${flex && "flex"} gap-${gap} ${direction} ${justify} ${items}`}>
+        <section className={`bg-gray-600 rounded font-bold text-xl h-[400px] w-[800px] border-4 ${flex && "flex"} ${gap} ${direction} ${justify} $c ${items}`}>
           <div className="bg-gray-900 w-16 h-16 border-4 flex justify-center items-center shadow-lg">1</div>
           <div className="bg-gray-900 w-16 h-16 border-4 flex justify-center items-center shadow-lg">2</div>
           <div className="bg-gray-900 w-16 h-16 border-4 flex justify-center items-center shadow-lg">3</div>        
         </section>      
       </div>
+
+      <pre className="mx-auto">
+        <code className="text-gray-100 text-lg">
+          &lt;div className= <span className="font-bold text-orange-300">"{flex && "flex"} {gap} {direction} {justify} {items}"</span> &gt;
+            <br />
+            <div className="ml-5 text-gray-300">
+              &lt;div&gt;1&lt;/div&gt;
+              <br />
+              &lt;div&gt;2&lt;/div&gt;
+              <br />
+              &lt;div&gt;3&lt;/div&gt;
+            </div>
+            
+          &lt;/div&gt;
+        </code>
+      </pre>
+
     </div>
   )
 }
